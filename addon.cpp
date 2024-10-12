0a48f6f0feb3dff9e7e90528645f3818135510ef
wignn <tigfiragnafatur1933@gmail.com>
2024-10-12 10:02:43 +0700

    first commit

--- addon.cpp
+++ addon.cpp
@@ -6,26 +6,63 @@ using v8::Local;
 using v8::String;
 using v8::Value;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  // Perbaikan penggunaan NewFromUtf8 dengan argumen tambahan
+void Method(const FunctionCallbackInfo<Value> &args)
+{
+  Isolate *isolate = args.GetIsolate();
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world", v8::NewStringType::kNormal)
                                 .ToLocalChecked());
 }
 
-void Sum(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  double a = 4.4563, b = 5.4563;
-  for (int i = 0; i < 1000000; i++) {
-    a += b;
+void sum(const FunctionCallbackInfo<Value> &args)
+{
+  Isolate *isolate = args.GetIsolate();
+  double a = 4.4563;
+
+  // Hitung bilangan prima
+  for (int i = 2; i < 1000000; i++)
+  {
+    bool isPrime = true;
+    for (int j = 2; j * j <= i; ++j)
+    {
+      if (i % j == 0)
+      {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime)
+    {
+      a += i;
+    }
   }
+
   args.GetReturnValue().Set(a);
 }
 
-void Initialize(Local<v8::Object> exports) {
-  NODE_SET_METHOD(exports, "hello", Method);
-  NODE_SET_METHOD(exports, "sum", Sum);
+void isPrismNumber(const FunctionCallbackInfo<Value> &args)
+{
+  Isolate *isolate = args.GetIsolate();
+  int n = args[0]->Int32Value(isolate->GetCurrentContext()).ToChecked();
+  int sum = 0;
+
+  for (int i = 1; i <= n; i++)
+  {
+    sum += i;
+    if (sum == n)
+    {
+      args.GetReturnValue().Set(true);
+      return;
+    }
+  }
+
+  args.GetReturnValue().Set(false);
 }
 
-// Perbaikan nama fungsi init menjadi Initialize
+void Initialize(Local<v8::Object> exports)
+{
+  NODE_SET_METHOD(exports, "hello", Method);
+  NODE_SET_METHOD(exports, "sum", sum);
+  NODE_SET_METHOD(exports, "isPrismNumber", isPrismNumber);
+}
+
 NODE_MODULE(addon, Initialize)
--- index.js
+++ index.js
@@ -1,18 +1,66 @@
-const addon = require('./build/Release/addon');n
+const addon = require('./build/Release/addon');
 
-function jsSum(){
+function jsSum() {
     let a = 5.4563, b = 9.5879;
-    for(let i = 0; i < 1000000; i++){
+    for (let i = 0; i < 1000000; i++) {
         a += b;
     }
 
+    function isPrismNumber(n) {
+        let sum = 0;
+        for (let i = 1; i <= n; i++) {
+            sum += i;
+            if (sum === n) return true;
+        }
+        return false;
+    }
+
+    let prismNumbers = [];
+    for (let i = 1; i <= 1000; i++) {
+        if (isPrismNumber(i)) {
+            prismNumbers.push(i);
+        }
+    }
+    console.log('Bilangan Prisma:', prismNumbers);
     let total = a;
     return total;
 }
 
-console.log("hello")
-console.log(addon.hello());
+console.log("JS");
 console.time('JS Time');
 console.log(jsSum());
-console.log("c++")
+console.timeEnd('JS Time');
+
+console.log("\n");
+
+console.log("C++");
+console.time('C++ Time');
 console.log(addon.sum());
+console.timeEnd('C++ Time');
+
+function printMatrixStyle(numbers) {
+    const matrixChar = '🔹'; 
+    const rows = process.stdout.columns; 
+
+    let output = '';
+    for (let number of numbers) {
+        output += `${matrixChar}${number.toString().padStart(3, ' ')} `;
+        if (output.length >= rows) {
+            console.log(output);
+            output = ''; 
+        }
+    }
+    if (output) {
+        console.log(output); 
+    }
+}
+
+console.log("C++ - Bilangan prima:");
+let cppPrismNumbers = [];
+for (let i = 1; i <= 1000; i++) {
+    if (addon.isPrismNumber(i)) {
+        cppPrismNumbers.push(i);
+    }
+}
+
+printMatrixStyle(cppPrismNumbers);
